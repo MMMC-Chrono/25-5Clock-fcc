0b473964ddd9f74056ab8ae17d1c1adb72c2e352
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-10 04:56:48 +0630

    added start_stop

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ class Clock extends React.Component{
         super(props);
         this.state = {
             break: 5,
-            section: 25
+            session: 25,
+            count: false
         };
         this.reset = this.reset.bind(this)
+        this.startStop = this.startStop.bind(this)
     }
     reset() {
         this.setState({
             break: 5,
-            section: 25
+            session: 25
         })
     }
-    render() {
+    startStop() {
+        this.setState({
+            count: true
+        })
+        console.log("start");
+    }
+    render() {        
         return (
             <div>
                 <div id="break-label">Break Length</div>
@@ -23,11 +31,11 @@ class Clock extends React.Component{
                 <button id="session-decrement"></button>
                 <button id="session-increment"></button>
                 <div id="break-length">{this.state.break}</div>
-                <div id="session-length">{this.state.section}</div>
+                <div id="session-length">{this.state.session}</div>
                 <div id="timer-label">Session</div>
                 <div id="time-left"></div>
-                <button id="start_stop"></button>
-                <button id="reset" onClick={this.reset}></button>
+                <button id="start_stop" onClick={this.startStop}>start_stop</button>
+                <button id="reset" onClick={this.reset}>reset</button>
             </div>
         )
     }
