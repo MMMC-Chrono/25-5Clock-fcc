c80f9dcbd962de4ad7d90078a0dcf777e546fe47
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-14 05:23:40 +0630

    added 0s and reset function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let counting;
 class Clock extends React.Component{
     constructor(props) {
         super(props);
@@ -11,10 +12,10 @@ class Clock extends React.Component{
         this.breakIncrement = this.breakIncrement.bind(this)
         this.sessionDecrement = this.sessionDecrement.bind(this)
         this.sessionIncrement = this.sessionIncrement.bind(this)
-        this.reset = this.reset.bind(this)
         this.startStop = this.startStop.bind(this)
-        
+        this.reset = this.reset.bind(this)        
     }
+    
     breakIncrement() {
         if (this.state.break < 60) {
             this.setState({
@@ -23,7 +24,7 @@ class Clock extends React.Component{
         }
     }
     breakDecrement() {
-        if (this.state.break > 0) {
+        if (this.state.break > 1) {
             this.setState({
                 break: this.state.break - 1
             })
@@ -38,7 +39,7 @@ class Clock extends React.Component{
         }       
     }
     sessionDecrement() {
-        if (this.state.session > 0) {
+        if (this.state.session > 1) {
             this.setState({
                 session: this.state.session - 1
             })
@@ -46,43 +47,58 @@ class Clock extends React.Component{
     }
 
     reset() {
+        clearInterval(counting);
         this.setState({
             break: 5,
             session: 25,
             count: false,
             reset: true
         })
+       /* let timeLeft = document.getElementById("time-left")
+        let minuteDisplay;
+        if (this.state.session < "10" || this.state.session === "0") {
+            timeLeft.innerHTML = "0" + this.state.session + ":" + "00";
+        } else {
+            timeLeft.innerHTML = this.state.session+ ":" + "00";
+        }  
+        
+        console.log("reset");
+        */
     }
+
     startStop() {
-        if ( this.state.count === false) {
+        if(!this.state.count) {
             this.setState({
                 count: true
-            })
-        } else {
-            this.setState({
-                count: false
-            })
-        }
-    }
-    render() {
-        let timeLeft = document.getElementById("time-left");
-        let minuteToSecond = this.state.session * 60000;
-        let countDownDate = new Date().getTime()+minuteToSecond;
-        if (this.state.count === true) {
-            var x = setInterval(function() {
+            });
+            let minuteToSecond = this.state.session * 60000;
+            let countDownDate = new Date().getTime() + minuteToSecond;
+            counting = setInterval(function() {
                 let now = new Date().getTime();    
                 let distance = countDownDate - now;    
                 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                if (minutes < 10) {
+                    minutes = "0" + minutes;
+                }
                 if (seconds < 10) {
                     seconds = "0" + seconds;
                 }
-                timeLeft.innerHTML = minutes + ":" + seconds;
+                if (minutes === "00" && seconds === "00") {
+                    clearInterval(counting);
+                }
+                document.getElementById("time-left").innerHTML = minutes + ":" + seconds;
             }, 1000);
+        } else {
+            this.setState({
+                count: false
+            });
+            clearInterval(counting);
         }
-        /*if (this.state.reset === true) {
-            timeLeft.innerHTML = this.state.session + ":" + "00"
-        }*/
+    }
+
+    render() {
+        
         return (
             <div>
                 <div id="break-label">Break Length</div>
@@ -103,7 +119,7 @@ class Clock extends React.Component{
                 <div id="session-length">{this.state.session}</div>
                 <div id="timer-label">Session</div>
                 <div id="time-left">{
-                    this.state.session <= "10"? "0" + this.state.session:
+                    this.state.session < "10"? "0" + this.state.session:
                     this.state.session === "0"? "0" + this.state.session: this.state.session
                 }:00</div>
                 <button id="start_stop" onClick={this.startStop}>start_stop</button>
