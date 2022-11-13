7d20e7d94bdfc5523b87b16fa803884e58c1ba03
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-14 01:51:48 +0630

    added increment decrement functions

--- app.js
+++ app.js
@@ -4,15 +4,53 @@ class Clock extends React.Component{
         this.state = {
             break: 5,
             session: 25,
-            count: false
+            count: false,
+            reset: false
         };
+        this.breakDecrement = this.breakDecrement.bind(this)
+        this.breakIncrement = this.breakIncrement.bind(this)
+        this.sessionDecrement = this.sessionDecrement.bind(this)
+        this.sessionIncrement = this.sessionIncrement.bind(this)
         this.reset = this.reset.bind(this)
         this.startStop = this.startStop.bind(this)
+        
     }
+    breakIncrement() {
+        if (this.state.break < 60) {
+            this.setState({
+                break: this.state.break + 1
+            })
+        }
+    }
+    breakDecrement() {
+        if (this.state.break > 0) {
+            this.setState({
+                break: this.state.break - 1
+            })
+        }
+    }
+
+    sessionIncrement() {
+        if (this.state.session < 60) {
+            this.setState({
+                session: this.state.session + 1
+            })
+        }       
+    }
+    sessionDecrement() {
+        if (this.state.session > 0) {
+            this.setState({
+                session: this.state.session - 1
+            })
+        }
+    }
+
     reset() {
         this.setState({
             break: 5,
-            session: 25
+            session: 25,
+            count: false,
+            reset: true
         })
     }
     startStop() {
@@ -25,9 +63,9 @@ class Clock extends React.Component{
                 count: false
             })
         }
-        console.log("start");
     }
     render() {
+        let timeLeft = document.getElementById("time-left");
         let minuteToSecond = this.state.session * 60000;
         let countDownDate = new Date().getTime()+minuteToSecond;
         if (this.state.count === true) {
@@ -36,20 +74,31 @@ class Clock extends React.Component{
                 let distance = countDownDate - now;    
                 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                if (seconds <= 10) {
+                if (seconds < 10) {
                     seconds = "0" + seconds;
                 }
-                document.getElementById("time-left").innerHTML = minutes + ":" + seconds;
+                timeLeft.innerHTML = minutes + ":" + seconds;
             }, 1000);
         }
+        /*if (this.state.reset === true) {
+            timeLeft.innerHTML = this.state.session + ":" + "00"
+        }*/
         return (
             <div>
                 <div id="break-label">Break Length</div>
+                <button id="break-decrement" onClick={this.breakDecrement} >
+                    <i className="fa-solid fa-minus"></i>
+                </button>
+                <button id="break-increment" onClick={this.breakIncrement} >
+                    <i className="fa-solid fa-plus"></i>
+                </button>
                 <div id="session-label">Session Length</div>
-                <button id="break-decrement"></button>
-                <button id="break-increment"></button>
-                <button id="session-decrement"></button>
-                <button id="session-increment"></button>
+                <button id="session-decrement" onClick={this.sessionDecrement} >
+                    <i className="fa-solid fa-minus"></i>
+                </button>
+                <button id="session-increment" onClick={this.sessionIncrement} >
+                    <i className="fa-solid fa-plus"></i>
+                </button>
                 <div id="break-length">{this.state.break}</div>
                 <div id="session-length">{this.state.session}</div>
                 <div id="timer-label">Session</div>
